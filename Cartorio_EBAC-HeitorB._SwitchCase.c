a3f5b4d4204d7cb1e07f05068ef1f02a651f323c
Heitor <loq.heitor@outlook.com>
2025-03-31 14:36:25 -0300

    Add files via upload

--- Cartorio_EBAC-HeitorB._SwitchCase.c
+++ Cartorio_EBAC-HeitorB._SwitchCase.c
@@ -11,11 +11,11 @@ int registro() //int é a variável utilizada para definir números inteiros
 	
 	//começo da criação das variáveis/strings
 	char arquivo[40]; //char é a variável utilizada para armazenar caracteres
-	char cpf[40];
-	char nome[40];
-	char sobrenome[40];
-	char cargo[40];
-	char telefone[40];
+	char cpf[40];    //criação da variável char de nome "cpf" com carga de 40 caracteres
+	char nome[40];	 //criação da variável char de nome "nome" com carga de 40 caracteres
+	char sobrenome[40]; //criação da variável char de nome "sobrenome" com carga de 40 caracteres
+	char cargo[40]; //criação da variável char de nome "cargo" com carga de 40 caracteres
+	char telefone[40]; //criação da variável char de nome "telefone" com carga de 40 caracteres
 	//final da criação de variáveis/strings
 	
 
@@ -24,7 +24,7 @@ int registro() //int é a variável utilizada para definir números inteiros
 	printf("        Registro de Usuários      \n\n\n");
 	
 	printf("Digite o CPF: "); // printf é a função responsável pela exibição na tela
-	scanf("%s", cpf); //função que lê o dado digitado pelo usuário
+	scanf("%s", cpf); //função que lê o valor/dado digitado pelo usuário
 	
 	strcpy(arquivo, cpf); //função Responsável por copiar o valor das strings
 	
@@ -89,14 +89,14 @@ int registro() //int é a variável utilizada para definir números inteiros
 	
 	getchar(); //função que lê a entrada do usuário através de caracteres
 	
-	char opcao; //criando a variável opção
-	opcao = getchar();
+	char opcao; //criando a variável de nome "opção"
+	opcao = getchar(); //definindo que "opção" é a escolha o usuário
 	
 	system("cls");
 	
 	if (opcao == '1') //se a condição for verdadeira, faça isso
 	{	
-		registro();
+		registro(); //voltar ao menu "Registros"
 	}
 	
 	else if (opcao == '2') // senão, faça isso
@@ -116,7 +116,7 @@ int registro() //int é a variável utilizada para definir números inteiros
 
 }
 
-int consulta()//função para consultar registros
+int consulta()// criando função de nome "consulta" para consultar registros
 {
 	setlocale(LC_ALL, "Portuguese");
 	
@@ -133,42 +133,77 @@ int consulta()//função para consultar registros
 	printf("        Consulta de Registro      \n\n");
 	
 	FILE *file;
-	file = fopen(cpf,"r"); //função para abrir o arquivo já salvo (r = read)
+	file = fopen(cpf,"r"); //função para abrir e ler o arquivo já salvo (r = read)
 	
 	
-	if(file == NULL) // se o arquivo não for localizado
+	if(file == NULL) // "if" -> funcção "se" = "if" se sim / "while" se não.. "file == NULL" -> se o arquivo não for localizado
 	{
 		printf("\n\tCPF Não Localizado!  \n");
 		printf("          Tente Novamente!");
 	}
 	
 	
-	while(fgets(conteudo, 200, file) != NULL) //imprimir na tela todos os dados do arquivo
+	while(fgets(conteudo, 200, file) != NULL) // "fgets" = buscar no banco de dados todos os dados do arquivo "!= NULL" até o final do arquivo
 	{
-		printf("%s", conteudo);
+		printf("%s", conteudo); //imprimir na tela informações do arquivo em questão
+	}
+
+	printf("\n\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
+	system("pause");
+	system("cls");
+	
+	printf("        Consulta de Registros      \n\n\n");	
+	printf("Deseja Consultar um Novo Registro? \n");
+	printf("\n\n    1 = SIM      2 = NÃO\n\n");
+	printf("Opção: ");
+	
+	
+	getchar(); //função que lê a entrada do usuário através de caracteres
+	
+	char opcao; //criando a variável de nome "opção"
+	opcao = getchar(); //definindo que "opção" é a escolha o usuário
+	
+	system("cls");
+	
+	if (opcao == '1') //se a condição for verdadeira, faça isso
+	{	
+		consulta(); //voltar ao menu "Consulta"
 	}
 	
-	printf("\n\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
-
+	else if (opcao == '2') // senão, faça isso
+	{
+		//apenas voltar ao menu principal
+	}
+	
+	else //se não for nenhum dos dois, faça isso
+	{
+		printf("        Consulta de Registros      \n\n\n");
+		printf("           Opção Inválida.\n");
+		printf("          Tente Novamente!");
+		printf("\n\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
+		
+		system("pause");
+	}
+	
 	fclose(file);
-	system("pause");
+
 }
 
-int exclusao()//Função responsável pela exclusão de registros
+int exclusao()// variável "int" de nome "exclusão" = Função responsável pela exclusão de registros
 {
 	char cpf[40];
 	
 	printf("        Exclusão de Registro      \n\n\n");
 	printf("Digite o CPF a ser Exlcuído: ");
 	scanf("%s", cpf);
+	getchar(); //Consumir o caractere "new line" após scanf
 	
-	FILE *file;
-	file = fopen(cpf,"r");
-	fclose(file);
-	
+	FILE *file; //abrindo pasta onde se localiza o arquivo 
+	file = fopen(cpf,"r"); //lendo banco de dados para validar dados inseridos pelo usuário
+		
 	system("cls");
 	
-	if (file == NULL)
+	if (file == NULL) //se não for encontrado
 	{
 		printf("        Exclusão de Registro      \n\n\n");
 		printf("\tCPF: ");
@@ -178,36 +213,42 @@ int exclusao()//Função responsável pela exclusão de registros
 		system("pause");
 		return 0; //comando para fazer o usuário retornar ao menu principal
 	}
-		
+
+	char registro [200];
+	
+	while(fgets(registro, 200, file) != NULL)
+	{
 	printf("        Exclusão de Registro      \n\n\n");	
 	printf("Deseja Excluir este Registro? \n");
 	printf("\tCPF: ");
-	printf("%s", cpf);
-	printf("\n\n    1 = SIM      2 = NÃO\n\n");
+	printf("%s", registro);
+	printf("\n\n    1 = SIM      2 = NÃO\n\n"); //validação para exclusão de dados
 	printf("Opção: ");
-	
+	}
+	fclose(file);
 	
 	getchar(); //função que lê a entrada do usuário através de caracteres
 	
 	char opcao; //criando a variável opção
-	opcao = getchar();
-	
+	opcao = getchar(); //definindo que "opção" = entrada de dados do usuário
+
+
 	system("cls");
 	
-	if (opcao == '1')
+	if (opcao == '1') //se a "opção" for igual à 1, fazer isso
 	{
-	if (remove(cpf) == 0){
+	if (remove(cpf) == 0){ //excluir dados do registro solicitado, no caso "cpf"
 	
 		printf("        Exclusão de Registro      \n\n\n");	
 		printf("\tCPF: ");
 		printf("%s", cpf);
-		printf("\n  Deletado com Sucesso!");
+		printf("\n  Deletado com Sucesso!"); //confirmação de exclusão de registro
 		printf("\n\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
-	}
 		system("pause");
 	}
+	}
 	
-	else if (opcao == '2')
+	else if (opcao == '2') //se a opção for igual à 2
 	{
 		printf("        Exclusão de Registro      \n\n\n");
 		printf("         Operação Cancelada!");
@@ -215,9 +256,10 @@ int exclusao()//Função responsável pela exclusão de registros
 		printf("\n\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
 		
 		system("pause");
+		
 	}
 	
-	else
+	else //se for diferente das opções acima
 	{
 		printf("        Exclusão de Registro      \n\n\n");
 		printf("           Opção Inválida.\n");
@@ -231,64 +273,84 @@ int exclusao()//Função responsável pela exclusão de registros
 
 int main() //Função do menú, começo do programa
 {
-	int opcao=0; //Definindo Variáveis
-	int repeticao=1;
-	
-	for(repeticao=1;repeticao=1;)
-	{
-		
-		system("cls"); //comando de limpar a tela
-	
-		setlocale(LC_ALL, "Portuguese"); //definindo biblioteca de linguagem 
+	setlocale(LC_ALL, "Portuguese"); //definindo biblioteca de linguagem 
 
-		printf("            Cartório da EBAC \n"); //início do menu
-		printf("           Seja Bem-Vinda (o)!\n\n\n");
-		printf("Escolha a opção desejada no menu abaixo:\n\n");
-		printf("\t1 - Registrar Alunos\n");
-		printf("\t2 - Consultar Registros de Alunos\n");
-		printf("\t3 - Deletar Registro de Aluno\n");
-		printf("\t4 - Encerrar Programa\n\n\n");
-		printf("Opção: "); //fim do menu
-		
-				
-		scanf("%d", &opcao); //armazenamento da escolha do usuário
-		
-		system("cls");
-		
-		switch(opcao)
+	//Definindo variáveis
+	int opcao=0; //variável "int" de nome "opção" com valor = 0
+	int repeticao=1; //variável "int" de nome "repeticao" com valor =1
+	char senhadigitada[]="a";
+	int comparacao;
+	
+	printf("            Cartório da EBAC \n");
+	printf("\n\n Login de Administrador \n\n Digite sua senha: ");
+	scanf("%s", senhadigitada);
+	
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	
+	
+	if(comparacao == 0)
+	{
+	
+		for(repeticao=1;repeticao=1;) //criando laço de repetição - enquanto "repeticao" = 1, manter o processo
 		{
-			case 1:
-			registro(); //chamada para a função registro
-			break;
-			
-			case 2:
-			consulta(); //chamada para a função consulta
-			break;
-			
-			case 3:
-			exclusao(); //chamada para a função exclusao
-			break;
-					
-			case 4:
-			printf("Obrigado por utilizar o sistema!!");
-			printf("\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
-			return 0; //chamada para quebrar o loopoing e encerrar
-			break;
+		
+			system("cls"); //comando de limpar a tela
+	
+			setlocale(LC_ALL, "Portuguese"); //definindo biblioteca de linguagem 
+
+			printf("            Cartório da EBAC \n"); //início do menu
+			printf("           Seja Bem-Vinda (o)!\n\n\n");
+			printf("Escolha a opção desejada no menu abaixo:\n\n");
+			printf("\t1 - Registro de Usuários \n");
+			printf("\t2 - Consultar Registros\n");
+			printf("\t3 - Excluir Registro\n");
+			printf("\t4 - Encerrar Programa\n\n\n");
+			printf("Opção: "); //fim do menu
+		
 				
+			scanf("%d", &opcao); //armazenamento da escolha do usuário
+		
+			system("cls");
+		
+			switch(opcao)
+			{
+				case 1:
+				registro(); //chamada para a função registro
+				break;
+				
+				case 2:
+				consulta(); //chamada para a função consulta
+				break;
+			
+				case 3:
+				exclusao(); //chamada para a função exclusao
+				break;
+					
+				case 4:
+				printf("\tObrigado por utilizar esse sistema!!");
+				printf("\n\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
+				return 0; //chamada para quebrar o loopoing e encerrar	
 						
-			default:
-			printf("            Cartório da EBAC \n\n\n");
-			printf("\tOpção Indisponível,");
-			printf("\nEscolha apenas entre as opções 1, 2 ou 3!");
-			printf("\n\n         Tente Novamente!!");
-			printf("\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
-			system("pause");
-			break;
+				default: //se o usuário digitar uma opção inválida
+				printf("            Cartório da EBAC \n\n\n");
+				printf("\tOpção Indisponível,");
+				printf("\nEscolha apenas entre as opções 1, 2, 3 ou 4!");
+				printf("\n\n         Tente Novamente!!");
+				printf("\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
+				system("pause");
+				break;
+			}//fim da seleção	
 		}
-		
-		
-		
-		} //fim da seleção
+	} 
 	
-	
+	else
+	{
+		system("cls");
+		printf("\t Senha Inválida\n\tTente Novamente!");
+		printf("\n\n\n\nEsse Software foi desenvolvido por Heitor B.®\n\n\n");
+		system("pause");
+		system("cls");
+		main();
 	}
+}
